f6acd23b53a3bd148351f6f83a9c70240a536656
LetsMakeMoney85 <Nathan_ST85@yahoo.com>
2026-07-22 06:23:11 -0700

    Add Contact component with community message

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section className="container page">
+      <h1>Contact</h1>
+      <p>
+        The NexusAI community is growing.
+      </p>
+    </section>
+  );
+}
