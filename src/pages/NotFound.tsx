d0966f8e8cd1917ff3f368733f7dd6e605252866
LetsMakeMoney85 <Nathan_ST85@yahoo.com>
2026-07-22 06:23:46 -0700

    Add NotFound component for 404 error page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <section className="container page">
+      <h1>404</h1>
+      <p>
+        This path does not exist.
+      </p>
+    </section>
+  );
+}
